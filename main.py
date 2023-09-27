6453b4decfc9a7207f6d30930ff7fd7da8f3eef9
Eunji Kim <eun_k_j@naver.com>
2023-09-27 16:03:02 +0900

    액세스 토큰을 사용한 로그인
    액세스 토큰이 있을 때만 상품 가져오기가 가능

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI, UploadFile, Form, Response
+from fastapi import FastAPI, UploadFile, Form, Response, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from typing import Annotated
 import sqlite3
 
 con = sqlite3.connect('db.db',check_same_thread=False)
 cur = con.cursor()
 
+# 서버에 테이블이 없을 시 생성
 cur.execute(f"""
             CREATE TABLE IF NOT EXISTS items (
 	            id INTEGER PRIMARY KEY,
@@ -22,6 +25,60 @@ cur.execute(f"""
 
 app = FastAPI()
 
+#SECRET 키는 중요!
+SERCRET = "super-coding"
+manager = LoginManager(SERCRET, '/login')
+
+# 유저 찾기
+@manager.user_loader()
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type(data) == dict:
+        WHERE_STATEMENTS = f'''id="{data['id']}"'''
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    user = cur.execute(f"""
+                       SELECT * from users WHERE {WHERE_STATEMENTS}'
+                       """).fetchone()
+    return user
+
+
+#로그인
+@app.post('/login')
+def login(id:Annotated[str,Form()],
+           password:Annotated[str,Form()]):
+    user = query_user(id)
+    if not user:
+        raise InvalidCredentialsException # 401을 자동으로 생성해서 내려줌
+    elif password != user['password']:
+        raise InvalidCredentialsException
+    
+    access_token = manager.create_access_token(data={
+        'sub' : {
+            'id':user['id'],
+            'name':user['name'],
+            'email':user['email']
+        }
+    })
+    
+    return {'access_token':access_token}
+
+
+#회원가입
+@app.post('/signup')
+def signup(id:Annotated[str,Form()],
+           password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]): # 이미 가입된 회원의 경우 막는 로직도 구성해볼것!
+    cur.execute(f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES('{id}', '{name}', '{email}','{password}')
+                """)
+    con.commit()
+    return '200'
+
+
+# 상품 업로드
 @app.post('/items')
 async def create_item(image:UploadFile,
                 title:Annotated[str,Form()],
@@ -40,16 +97,21 @@ async def create_item(image:UploadFile,
     con.commit()
     return '200'
 
+
+# 상품 목록 보여주기
 @app.get('/items')
-async def get_items():
+async def get_items(user=Depends(manager)):
     # 컬렴명도 같이 가져옴
     con.row_factory = sqlite3.Row
     cur = con.cursor()
     rows = cur.execute(f"""
                        SELECT * from items;
                        """).fetchall()
+    
     return JSONResponse(jsonable_encoder(dict(row) for row in rows))
 
+
+# 상품이미지 가져오기
 @app.get('/images/{item_id}')
 async def get_image(item_id):
     cur = con.cursor()
@@ -60,15 +122,5 @@ async def get_image(item_id):
     
     return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
 
-@app.post('/signup')
-def signup(id:Annotated[str,Form()], password:Annotated[str,Form()],
-           name:Annotated[str,Form()],
-           email:Annotated[str,Form()]): # 이미 가입된 회원의 경우 막는 로직도 구성해볼것!
-    cur.execute(f"""
-                INSERT INTO users(id,name,email,password)
-                VALUES('{id}', '{name}', '{email}','{password}')
-                """)
-    con.commit()
-    return '200'
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
